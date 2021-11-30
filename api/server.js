d54eee30ffdc1464bb689ee38c715afbe1a4179c
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-11-29 17:10:33 -0800

    first two endpoints done

--- api/server.js
+++ api/server.js
@@ -1,9 +1,60 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
+const Users = require('./users/model')
 
 const server = express()
 
+
 server.use(express.json())
+
+// Endpoints
+
+// create new user
+server.post('/api/users', (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+        Users.insert(req.body)
+        .then(newUser => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the user to the database"
+            })
+        })
+    }
+})
+
+// get all users
+server.get(`/api/users`, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The users information could not be retrieved"
+            })
+        })
+})
+
+// get user by id
+server.get('/api/users/:id', (req, res) => {
+
+})
+
+// delete user by id
+server.delete('/api/users/:id', (req, res) => {
+
+})
+
+// update user by id
+server.put('/api/users/:id', (req, res) => {
+
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const port = 9000;
 
 // START YOUR SERVER HERE
 server.listen(port, ()=> {
-    console.log('server listening on 9000')
+    console.log(`server listening on ${port}`)
 })
+
