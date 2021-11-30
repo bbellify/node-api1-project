74c0c217098940406e07aadb4675435c0d438b4d
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-11-29 17:25:28 -0800

    delete endpoint working

--- api/server.js
+++ api/server.js
@@ -62,6 +62,21 @@ server.get('/api/users/:id', (req, res) => {
 
 // delete user by id
 server.delete('/api/users/:id', (req, res) => {
+    Users.remove(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            } else {
+                res.json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The user could not be removed"
+            })
+        })
 
 })
 
